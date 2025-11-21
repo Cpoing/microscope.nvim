26df8c04bc07b256a526a2fbf1ca4f619c2b92db
Ted Lee <tedlee000@hotmail.com>
2025-11-21 00:34:46 -0500

    encoding param

--- lua/microscope/lsp.lua
+++ lua/microscope/lsp.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 function M.get_definition(callback)
-	local params = vim.lsp.util.make_position_params()
+	local params = vim.lsp.util.make_position_params(0, 'utf-8')
 
 	vim.lsp.buf_request(0, "textDocument/definition", params, function(err, result)
 		if err or not result or vim.tbl_isempty(result) then
