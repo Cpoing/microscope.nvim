56b607aab11a8d6466711754905a474c001a3894
Ted Lee <tedlee000@hotmail.com>
2025-11-30 13:11:25 -0500

    removed keybinds

--- lua/microscope/init.lua
+++ lua/microscope/init.lua
@@ -3,14 +3,29 @@ local lsp = require("microscope.lsp")
 
 local M = {}
 
+M.options = {
+  keymaps = {},
+}
+
+function M.setup(opts)
+  M.options = vim.tbl_deep_extend("force", M.options, opts or {})
+
+  local key = M.options.keymaps.preview_definition
+  if key then
+    vim.keymap.set("n", key, function()
+      M.preview_definition()
+    end, { desc = "Microscope: Preview Definition" })
+  end
+end
+
 function M.preview_definition(opts)
-	lsp.get_definition(function(def)
-		if not def then
-			vim.cmd([[echohl ErrorMsg | echom "Definition not found" | echohl None]])
-			return
-		end
-		float.open_float(def.filepath, def.line)
-	end)
+  lsp.get_definition(function(def)
+    if not def then
+      vim.cmd([[echohl ErrorMsg | echom "Definition not found" | echohl None]])
+      return
+    end
+    float.open_float(def.filepath, def.line)
+  end)
 end
 
 return M
